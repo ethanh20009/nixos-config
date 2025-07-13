93d79acdeed637b33f95d65a515d417a49a02448
Ethan Howard <ethanh20009@outlook.com>
2025-07-13 13:47:48 +0100

    add hyprlock theming

--- modules/hyprland/hyprlock.nix
+++ modules/hyprland/hyprlock.nix
@@ -1,4 +1,9 @@
-{...}: {
+{config, ...}: let
+  # Assign a shorthand for the Stylix generated palette
+  # When using stylix.image, the generated colors are typically in config.stylix.generated.palette
+  # If you were using stylix.base16Scheme, it would be config.lib.stylix.colors
+  stylixColors = config.stylix.generated.palette;
+in {
   programs.hyprlock = {
     enable = true;
     settings = {
@@ -27,6 +32,16 @@
       #     shadow_passes = 2;
       #   }
       # ];
+
+      label = with stylixColors; [
+        {
+          size = "200, 50";
+          position = "0, 100";
+          monitor = "";
+          text = "cmd[update:1000] echo \"<span foreground='##${base05}'>$(date +\"%a %d %b %H:%M\")</span>\"";
+          font_size = 20;
+        }
+      ];
     };
   };
 }
