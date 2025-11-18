427ef753114c7b5f3931198b9a16a514f3718e6a
Ethan Howard <ethanh20009@outlook.com>
2025-11-18 15:56:50 +0000

    fix deno vtsls conflict

--- modules/nvf/nvf.nix
+++ modules/nvf/nvf.nix
@@ -612,6 +612,31 @@ in {
         servers =
           {
             vtsls = {
+              root_dir =
+                lib.generators.mkLuaInline
+                /*
+                lua
+                */
+                ''
+                  function(bufnr, on_dir)
+                      -- The project root is where the LSP can be started from
+                      -- As stated in the documentation above, this LSP supports monorepos and simple projects.
+                      -- We select then from the project root, which is identified by the presence of a package
+                      -- manager lock file.
+                      local isDenoProject = vim.fs.root(bufnr, {'deno.json', 'deno.jsonc'})
+                      if isDenoProject then
+                        return nil
+                      end
+                      local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
+                      -- Give the root markers equal priority by wrapping them in a table
+                      root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers, { '.git' } }
+                        or vim.list_extend(root_markers, { '.git' })
+                      -- We fallback to the current working directory if no project root is found
+                      local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd()
+
+                      on_dir(project_root)
+                    end
+                '';
               settings = {
                 vtsls = {
                   enableMoveToFileCodeAction = true;
@@ -666,17 +691,17 @@ in {
               };
             };
           }
-          # // lib.optionalAttrs myConfig.extras.deno.enable {
-          #   denols = {
-          #     root_markers = [
-          #       "deno.json"
-          #       "deno.jsonc"
-          #     ];
-          #     single_file_support = false;
-          #     settings = {};
-          #   };
-          # }
-          ;
+          // lib.optionalAttrs myConfig.extras.deno.enable {
+            denols = {
+              root_markers = [
+                "deno.json"
+                "deno.jsonc"
+              ];
+              workspace_required = true;
+              single_file_support = false;
+              settings = {};
+            };
+          };
       };
 
       visuals = {
