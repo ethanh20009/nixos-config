30cdd234e435a91551b42de9c328e81b0a54ede0
Ethan Howard <ethanh20009@outlook.com>
2026-05-18 21:00:45 +0100

    refactor nvf options

---
+++ modules/nvf/options.nix
@@ -0,0 +1,14 @@
+{lib, ...}: {
+  options.myConfig.nvf = {
+    companionLocal = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable local companion (Claude Code) settings";
+    };
+    companion = lib.mkOption {
+      default = "gemini";
+      type = lib.types.enum ["gemini" "claude"];
+      description = "Which code companion to use. Available: gemini, claude";
+    };
+  };
+}
--- modules/options.nix
+++ modules/options.nix
@@ -1,4 +1,8 @@
 {lib, ...}: {
+  imports = [
+    ./nvf/options.nix
+  ];
+
   options.myConfig = {
     browser = lib.mkOption {
       type = lib.types.str;
@@ -104,19 +108,6 @@
       description = ''
         Install Tex system wide
       '';
-    };
-
-    nvf = {
-      companionLocal = lib.mkOption {
-        type = lib.types.bool;
-        default = false;
-        description = "Enable local companion (Claude Code) settings";
-      };
-      companion = lib.mkOption {
-        default = "gemini";
-        type = lib.types.enum ["gemini" "claude"];
-        description = "Which code companion to use. Available: gemini, claude";
-      };
     };
   };
 }
